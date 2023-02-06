d293e3bbf12f7602ec9f6613cf57c311df4d875a
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-06 18:39:25 +0300

    recipe model updated

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id' ,class_name: 'User'
-  has_many :foods, through: :recipe_foods, dependent: :destroy
   has_many :recipe_foods
+  has_many :foods, through: :recipe_foods, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 5 }
   validates :preparation_time, presence: true
