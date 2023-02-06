64174f51393ffc106b154ecc456e488890d74712
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-06 17:56:31 +0300

    recipe model updated

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: 'user_id' ,class_name: 'User'
+  has_many :recipe_foods
+  has_many :foods, through: :recipe_foods, dependent: :destroy
 end
