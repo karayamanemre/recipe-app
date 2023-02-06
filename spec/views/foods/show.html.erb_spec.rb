8a832931912f1074aa3784bcaad07727406683d8
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-06 08:59:32 +0000

    Create Food MVC

---
+++ spec/views/foods/show.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "foods/show", type: :view do
+  before(:each) do
+    assign(:food, Food.create!(
+      name: "Name",
+      measurement_unit: "Measurement Unit",
+      price: "",
+      quantity: 2,
+      user: nil
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Measurement Unit/)
+    expect(rendered).to match(//)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(//)
+  end
+end
