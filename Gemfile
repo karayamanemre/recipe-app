7b0710f878cc1b2bed4266160b44ad8588cdbbad
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-06 09:21:38 +0000

    Setup authentication with devise

--- Gemfile
+++ Gemfile
@@ -63,6 +63,8 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
 
+  gem "letter_opener"
+
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -71,3 +73,5 @@ group :development do
 end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  authenticated :user do
+    root to: redirect("recipes#index"), as: :authenticated_root
+  end
+  unauthenticated do
+    root to: redirect("/public_recipes"), as: :unauthenticated_root
+  end
+
+  match "/public_recipes", to: "recipes#index", via: :get
+
   resources :recipe_foods
   resources :recipes
   resources :foods
