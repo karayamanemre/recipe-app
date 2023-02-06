724b38f7e4139961780a371fb1521d22df536533
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-06 16:02:43 +0300

    linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: %i[show edit update destroy]
 
   def index
     @users = User.all
@@ -22,8 +22,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :recipes , foreign_key: 'user_id'
-  has_many :foods , foreign_key: 'user_id'
+  has_many :recipes, foreign_key: 'user_id'
+  has_many :foods, foreign_key: 'user_id'
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
