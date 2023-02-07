65a8bbcce33bebe2cb4b4d3b289c989dbb15d967
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-07 00:31:04 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,9 @@ Rails.application.routes.draw do
 
   match "/public_recipes", to: "recipes#index", via: :get
 
-  resources :recipe_foods
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+  end
   resources :foods
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
