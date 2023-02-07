f1384216e021b0ea530c5970e1320cf2fb90630c
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-07 00:39:34 +0000

    Add spec for recipe_food model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,5 +2,7 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
 
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_or_equal_to: 0 }
+  validates :recipe, presence: true
+  validates :food, presence: true
 end
