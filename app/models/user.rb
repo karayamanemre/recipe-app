0d70cb8de1f130837b64bd68b66bb8ab13783051
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-08 09:34:19 +0000

    Add method to generate shopping list

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,25 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :encrypted_password, presence: true
+
+  def shopping_list(recipe)
+    recipe = Recipe.find(recipe.id)
+    recipe_foods = RecipeFood.where(recipe_id: recipe.id).includes(:food)
+    missing_foods = []
+    total_val = 0
+    total_ingredients = 0
+
+    food_quantities = Food.where(id: recipe_foods.map(&:food_id)).pluck(:id, :quantity).to_h
+
+    recipe_foods.each do |ingredient|
+      food_quantity = food_quantities[ingredient.food_id]
+      next if food_quantity && (ingredient.quantity <= food_quantity)
+
+      total_val += (ingredient.quantity - food_quantity.to_i) * ingredient.food.price
+      total_ingredients += 1
+      missing_foods << ingredient
+    end
+
+    [total_ingredients, total_val, missing_foods]
+  end
 end
