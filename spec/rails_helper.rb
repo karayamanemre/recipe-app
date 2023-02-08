f47174f313b0cc9cffcd4c774f232639fc5c7db0
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-08 10:45:35 +0000

    Fixing testing errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,8 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  config.include Devise::Test::IntegrationHelpers, type: :request # to sign_in user by Devise
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe RecipesController, type: :request do
   before(:each) do
     default_url_options[:host] = 'localhost:3000'
+    @user = User.create(name: 'John Smith', email: 'john.smith1@gmail.com', password: 'password', password_confirmation: 'password', confirmed_at: Time.now)
+    sign_in @user
   end
   describe 'GET /recipes' do
     it 'returns a success response' do
