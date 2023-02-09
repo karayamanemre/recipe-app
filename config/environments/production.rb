86e0bcb4174c29a0073c85d4cc7594665f508b1f
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-02-09 09:35:18 +0000

    Set default_url_options[:host]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options[:host] = 'recipe-app-8dxv.onrender.com'
+
